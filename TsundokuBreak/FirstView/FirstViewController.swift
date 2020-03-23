821a33200285faa54ee6aa0b260f7b40518de053
mizukick <mizukijan@gmail.com>
2020-03-23 18:25:12 +0900

    storyboardの削除

---
+++ TsundokuBreak/FirstView/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  TsundokuBreak
+//
+//  Created by Mizuki Kubota on 2020/03/23.
+//  Copyright © 2020 MizukiKubota. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
