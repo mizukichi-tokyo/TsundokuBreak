ae397c6462972fc7a1f39f4198eeaabce9e6cb24
mizukick <mizukijan@gmail.com>
2020-03-24 13:16:59 +0900

    Google Books APIs のStructを追加 #1

---
+++ TsundokuBreak/Struct/GoogleBooksAPIs.swift
@@ -0,0 +1,96 @@
+//
+//  GoogleBooksAPIs.swift
+//  TsundokuBreak
+//
+//  Created by Mizuki Kubota on 2020/03/24.
+//  Copyright © 2020 MizukiKubota. All rights reserved.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let googleBooksAPIs = try? newJSONDecoder().decode(GoogleBooksAPIs.self, from: jsonData)
+
+import Foundation
+
+// MARK: - GoogleBooksAPIs
+struct GoogleBooksAPIs: Codable {
+    let kind: String?
+    let totalItems: Int?
+    let items: [Item]?
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let kind, id, etag: String?
+    let selfLink: String?
+    let volumeInfo: VolumeInfo?
+    let saleInfo: SaleInfo?
+    let accessInfo: AccessInfo?
+    let searchInfo: SearchInfo?
+}
+
+// MARK: - AccessInfo
+struct AccessInfo: Codable {
+    let country, viewability: String?
+    let embeddable, publicDomain: Bool?
+    let textToSpeechPermission: String?
+    let epub, pdf: Epub?
+    let webReaderLink: String?
+    let accessViewStatus: String?
+    let quoteSharingAllowed: Bool?
+}
+
+// MARK: - Epub
+struct Epub: Codable {
+    let isAvailable: Bool?
+}
+
+// MARK: - SaleInfo
+struct SaleInfo: Codable {
+    let country, saleability: String?
+    let isEbook: Bool?
+}
+
+// MARK: - SearchInfo
+struct SearchInfo: Codable {
+    let textSnippet: String?
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title, subtitle: String?
+    let authors: [String]?
+    let publishedDate, volumeInfoDescription: String?
+    let industryIdentifiers: [IndustryIdentifier]?
+    let readingModes: ReadingModes?
+    let pageCount: Int?
+    let printType, maturityRating: String?
+    let allowAnonLogging: Bool?
+    let contentVersion: String?
+    let imageLinks: ImageLinks?
+    let language: String?
+    let previewLink, infoLink: String?
+    let canonicalVolumeLink: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title, subtitle, authors, publishedDate
+        case volumeInfoDescription = "description"
+        case industryIdentifiers, readingModes, pageCount, printType, maturityRating, allowAnonLogging, contentVersion, imageLinks, language, previewLink, infoLink, canonicalVolumeLink
+    }
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String?
+}
+
+// MARK: - IndustryIdentifier
+struct IndustryIdentifier: Codable {
+    let type, identifier: String?
+}
+
+// MARK: - ReadingModes
+struct ReadingModes: Codable {
+    let text, image: Bool?
+}
