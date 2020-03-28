de09a17dd52ee0fa292ab3c5ad20a83c62805fab
mizukick <mizukijan@gmail.com>
2020-03-28 20:59:58 +0900

    タイトルラベルを太字にした	#4

--- TsundokuBreak/View/TabView/TabViewController.swift
+++ TsundokuBreak/View/TabView/TabViewController.swift
@@ -14,6 +14,8 @@ class TabViewController: ColorMatchTabsViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.font = UIFont.boldSystemFont(ofSize: titleLabel.font.pointSize)
+
         colorMatchTabDataSource = self
     }
 
