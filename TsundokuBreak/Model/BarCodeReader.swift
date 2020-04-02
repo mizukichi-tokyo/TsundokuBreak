1880ef8871c49a0a51164779ca25267375126e45
mizukick <mizukijan@gmail.com>
2020-04-02 22:39:41 +0900

    isbnからGoogleBooksAPIsの呼び出し #10

--- TsundokuBreak/Model/BarCodeReader.swift
+++ TsundokuBreak/Model/BarCodeReader.swift
@@ -9,6 +9,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Moya
 
 struct  BarCodeReaderModelInput {
     let isbnRelay: PublishRelay<String>
@@ -28,16 +29,42 @@ final class  BarCodeReaderModel: BarCodeReaderModelType, Injectable, BarCodeRead
     var outputs: BarCodeReaderModelOutput?
 
     private let disposeBag = DisposeBag()
+    private let provider = MoyaProvider<GoogleBooksAPIs>()
 
     init(with dependency: Dependency) {
         self.outputs = self
     }
 
     func setup(input: BarCodeReaderModelInput) {
-        input.isbnRelay.subscribe(onNext: { isbn in
-            print(isbn)
+        input.isbnRelay.subscribe(onNext: { [weak self] isbn in
+            guard let self = self else { return }
+            self.getRequest(isbnNumber: isbn)
         }).disposed(by: disposeBag)
 
     }
 
+}
+
+extension BarCodeReaderModel {
+    private func getRequest(isbnNumber: String) {
+        var isbnString: String = "isbn:"
+        isbnString += isbnNumber
+
+        provider.request(.search( isbnString )) { result in
+            switch result {
+            case let .success(moyaResponse):
+                do {
+                    let data = try moyaResponse.mapJSON()
+                    print(data)
+                    let jsonData = try? JSONDecoder().decode(BookInfo.self, from: moyaResponse.data)
+                    print(jsonData?.items?[0].volumeInfo?.title as Any)
+                } catch {
+                    print("json parse失敗")
+                }
+            case let .failure(error):
+                print("アクセスに失敗しました:\(error)")
+            }
+
+        }
+    }
 }
