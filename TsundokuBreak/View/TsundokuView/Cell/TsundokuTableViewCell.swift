d1ecbd3496cdaa75ee10119db5fa308d6d4004cd
mizukick <mizukijan@gmail.com>
2020-04-09 20:40:41 +0900

    カスタムセルの追加 #12

---
+++ TsundokuBreak/View/TsundokuView/Cell/TsundokuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TsundokuTableViewCell.swift
+//  TsundokuBreak
+//
+//  Created by Mizuki Kubota on 2020/04/09.
+//  Copyright © 2020 MizukiKubota. All rights reserved.
+//
+
+import UIKit
+
+class TsundokuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
